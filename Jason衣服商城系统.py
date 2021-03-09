c0a15cd77a4e0c0b223cf465dd3c9c1f1459948c
MasterGaol <80317946+MasterGaol@users.noreply.github.com>
2021-03-09 17:45:29 +0800

    Add files via upload

---
+++ Jason衣服商城系统.py
@@ -0,0 +1,40 @@
+id1 = 1
+name1 = "短袖"
+price1 = 50
+quanlity1 = "A+"
+num1 = 200
+type1 = "上衣"
+sales1 = 50
+
+id2 = 1
+name2 = "短裤"
+price2 = 40
+quanlity2 = "B+"
+num2 = 300
+type2 = "裤子"
+sales2 = 20
+
+id3 = 1
+name3 = "内裤"
+price3 = 10
+quanlity3 = "A+"
+num3 = 400
+type3 = "裤子"
+sales3 = 30
+
+id4 = 1
+name4 = "外套"
+price4 = 100
+quanlity4 = "C+"
+num4 = 500
+type4 = "上衣"
+sales4 = 40
+print("---------------------------------")
+print("          Jason衣服商城系统       ")
+print("---------------------------------")
+print("编号   名称  价格  品质   数量    类型   销量")
+print (id1,"   ",name1,"   ",price1,"   ",quanlity1,"   ",num1,"   ",type1,"   ",sales1,"    ")
+print (id2,"   ",name2,"   ",price2,"   ",quanlity2,"   ",num2,"   ",type2,"   ",sales2,"    ")
+print (id3,"   ",name3,"   ",price3,"   ",quanlity3,"   ",num3,"   ",type3,"   ",sales3,"    ")
+print (id4,"   ",name4,"   ",price4,"   ",quanlity4,"   ",num4,"   ",type4,"   ",sales4,"    ")
+print("总金额:",(price1 * num1 + price2 * num2 + price3 * num3 + price4 * num4),"￥!")
